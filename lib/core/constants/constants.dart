9ce0762d2437a1cf1329dc0d7570020c6a6f8ff4
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-05-25 00:31:19 +0600

    remote data source added

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,4 @@
+const String newApiBaseUrl = 'https://newsapi.org/v2';
+const String newsApiKey = '0c4cbd1db54b4882a0dbfa8a10f7a433';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
